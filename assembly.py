e93d05c26663d0b37c8fbd62e8fccf97c84d23db
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-04-18 12:25:15 -0600

    wrote all my commands

--- assembly.py
+++ assembly.py
@@ -4,10 +4,29 @@ from mimetypes import common_types
 
 
 ARMInstructions = """
-mov r1, 0
-loop:
-ldrb r2, [a1], 1
-cmp r2, 0
+MOVW R4, 0
+MOVT R4, 0x3F20
+ADD R2, R4, 0x08
+LDR R3,(R2)
+ORR R3, R3, 0x00000008
+STR R3, (R2)
+ADD R3, R4, 0x1c
+MOVW R2, 0x0000
+MOVT R2, 0x0020
+STR R2, (R3)
+MOVW R5, 0xC3500
+MOVT r5, 0xC
+SUBS R5, R5, 1
+(addBranch Command)
+ADD R3, R4, 28
+MOVW R2, 0x0000
+MOVT R2, 0x0020
+STR R2, (R3)
+MOVW R5, 0xC3500
+MOVT R5,0xC
+SUBS R5, R5, 1
+(add branch to go back 3)
+(add branch to go back 24)
 """
 
 class Assembler():
